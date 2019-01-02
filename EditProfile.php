f935df7943f5a5408641c1351d83254e2d2e499d
deadhead1316 <deadhead1316@users.noreply.github.com>
2019-01-02 23:28:55 +0600

    2nd

--- EditProfile.php
+++ EditProfile.php
@@ -20,6 +20,7 @@
     <head>
         <title><?php echo strtoupper($username);?></title>
         <link rel="stylesheet" type="text/css" href="css/editprofile.css">
+        <script type="text/javascript" src="js/editprofie.js"></script>
     </head>
     <body>
         <header>
@@ -43,7 +44,7 @@
             </div>
         </header>
         <div class = "info_content">
-            <img src="../image/avatar/smoke.png" class="info_content_avatar" onClick="chooseProPicDivShow()">
+            <img src="../image/avatar/smoke.png" id="avatar1" class="info_content_avatar" onClick="chooseProPicDivShow()">
             <br>
             <br>
             <br>
@@ -61,40 +62,57 @@
 
             
         </div>
-        <script>
-            function chooseProPicDivShow(){
-                document.getElementById("choose_pic_div").style.visibility = 'visible';
-            }
-            function chooseProPicDivHide(){
-                document.getElementById("choose_pic_div").style.visibility = 'hidden';
-            }
-        </script>
+        
         <div id="choose_pic_div" class = "wow">
             <h1>Choose your profile picture....<h1>
             <br>
-            <img src="../image/avatar/smoke.png" alt="smoke" class="avatar_choose " onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/bandit.png" alt="bandit" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/blitz.png" alt="blitz" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/castle.png" alt="castle" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/caviera.png" alt="caviera" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/doc.png" alt="doc" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/fuze.png" alt="fuze" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/glaz.png" alt="glaz" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/iq.png" alt="iq" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/jackle.png" alt="jackle" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/jager.png" alt="jager" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/montagne.png" alt="montagne" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/mute.png" alt="mute" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/recrute.png" alt="recrute" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/rook.png" alt="rook" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/sledge.png" alt="sledge" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/smoke.png" alt="smoke" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/thatchar.png" alt="thatchar" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/thermite.png" alt="thermite" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/valkyrie.png" alt="valkyrie" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/blackbeard.png" alt="blackbeard" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/kapkan.png" alt="kapkan" class="avatar_choose" onClick = "chooseProPicDivHide()">
-            <img src="../image/avatar/twitch.png" alt="twitch" class="avatar_choose" onClick = "chooseProPicDivHide()">
+            <img src="../image/avatar/smoke.png" id ="smoke" alt="smoke" class="avatar_choose " onClick = "changeProfilePicture('smoke')">
+            <img src="../image/avatar/bandit.png" id ="bandit" alt="bandit" class="avatar_choose" onClick = "changeProfilePicture('bandit')">
+            <img src="../image/avatar/blitz.png" id="blitz" alt="blitz" class="avatar_choose" onClick = "changeProfilePicture('blitz')">
+            <img src="../image/avatar/castle.png" id="castle" alt="castle" class="avatar_choose" onClick = "changeProfilePicture('castle')">
+            <img src="../image/avatar/caviera.png" id="caviera" alt="caviera" class="avatar_choose" onClick = "changeProfilePicture('caviera')">
+            <img src="../image/avatar/doc.png" id= "doc" alt="doc" class="avatar_choose" onClick = "changeProfilePicture('doc')">
+            <img src="../image/avatar/fuze.png" id="fuze" alt="fuze" class="avatar_choose" onClick = "changeProfilePicture('fuze')">
+            <img src="../image/avatar/glaz.png" id="glaz" alt="glaz" class="avatar_choose" onClick = "changeProfilePicture('glaz')">
+            <img src="../image/avatar/iq.png" id="iq" alt="iq" class="avatar_choose" onClick = "changeProfilePicture('iq')">
+            <img src="../image/avatar/jackle.png" id="jackle" alt="jackle" class="avatar_choose" onClick = "changeProfilePicture('jackle')">
+            <img src="../image/avatar/jager.png" id="jager" alt="jager" class="avatar_choose" onClick = "changeProfilePicture('jager')">
+            <img src="../image/avatar/montagne.png" id="montagne" alt="montagne" class="avatar_choose" onClick = "changeProfilePicture('montagne')">
+            <img src="../image/avatar/mute.png" id="mute" alt="mute" class="avatar_choose" onClick = "changeProfilePicture('mute')">
+            <img src="../image/avatar/recrute.png" id="recrute" alt="recrute" class="avatar_choose" onClick = "changeProfilePicture('recrute')">
+            <img src="../image/avatar/rook.png" id="rook" alt="rook" class="avatar_choose" onClick = "changeProfilePicture('rook')">
+            <img src="../image/avatar/sledge.png" id="sledge" alt="sledge" class="avatar_choose" onClick = "changeProfilePicture('sledge')">
+            <img src="../image/avatar/thatchar.png" id="thatchar" alt="thatchar" class="avatar_choose" onClick = "changeProfilePicture('thatchar')">
+            <img src="../image/avatar/thermite.png" id="thermite" alt="thermite" class="avatar_choose" onClick = "changeProfilePicture('thermite')">
+            <img src="../image/avatar/valkyrie.png" id="valkyrie" alt="valkyrie" class="avatar_choose" onClick = "changeProfilePicture('valkyrie')">
+            <img src="../image/avatar/blackbeard.png" id="blackbeard" alt="blackbeard" class="avatar_choose" onClick = "changeProfilePicture('blackbeard')">
+            <img src="../image/avatar/kapkan.png" id="kapkan" alt="kapkan" class="avatar_choose" onClick = "changeProfilePicture('kapkan')">
+            <img src="../image/avatar/ash.png" id="ash" alt="ash" class="avatar_choose" onClick = "changeProfilePicture('ash')">
+            <img src="../image/avatar/alibi.png" id="alibi" alt="alibi" class="avatar_choose" onClick = "changeProfilePicture('alibi')">
+            <img src="../image/avatar/buck.png" id="buck" alt="buck" class="avatar_choose" onClick = "changeProfilePicture('buck')">
+            <img src="../image/avatar/capitao.png" id="capitao" alt="capitao" class="avatar_choose" onClick = "changeProfilePicture('capitao')">
+            <img src="../image/avatar/clash.png" id="clash" alt="clash" class="avatar_choose" onClick = "changeProfilePicture('clash')">
+            <img src="../image/avatar/dokkaebi.png" id="dokkaebi" alt="dokkaebi" class="avatar_choose" onClick = "changeProfilePicture('dokkaebi')">
+            <img src="../image/avatar/echo.png" id="echo" alt="echo" class="avatar_choose" onClick = "changeProfilePicture('echo')">
+            <img src="../image/avatar/ela.png" id="ela" alt="ela" class="avatar_choose" onClick = "changeProfilePicture('ela')">
+            <img src="../image/avatar/finka.png" id="finka" alt="finka" class="avatar_choose" onClick = "changeProfilePicture('finka')">
+            <img src="../image/avatar/frost.png" id="frost" alt="frost" class="avatar_choose" onClick = "changeProfilePicture('frost')">
+            <img src="../image/avatar/kaid.png" id="kaid" alt="kaid" class="avatar_choose" onClick = "changeProfilePicture('kaid')">
+            <img src="../image/avatar/lion.png" id="lion" alt="lion" class="avatar_choose" onClick = "changeProfilePicture('lion')">
+            <img src="../image/avatar/lision.png" id="lision" alt="lision" class="avatar_choose" onClick = "changeProfilePicture('lision')">
+            <img src="../image/avatar/maestro.png" id="maestro" alt="maestro" class="avatar_choose" onClick = "changeProfilePicture('maestro')">
+            <img src="../image/avatar/mavrick.png" id="mavrick" alt="mavrick" class="avatar_choose" onClick = "changeProfilePicture('mavrick')">
+            <img src="../image/avatar/mira.png" id="mira" alt="mira" class="avatar_choose" onClick = "changeProfilePicture('mira')">
+            <img src="../image/avatar/nomad.png" id="nomad" alt="nomad" class="avatar_choose" onClick = "changeProfilePicture('nomad')">
+            <img src="../image/avatar/pulse.png" id="pulse" alt="pulse" class="avatar_choose" onClick = "changeProfilePicture('pulse')">
+            <img src="../image/avatar/tachanka.png" id="tachanka" alt="tachanka" class="avatar_choose" onClick = "changeProfilePicture('tachanka')">
+            <img src="../image/avatar/vigil.png" id="vigil" alt="vigil" class="avatar_choose" onClick = "changeProfilePicture('vigil')">
+            <img src="../image/avatar/ying.png" id="ying" alt="ying" class="avatar_choose" onClick = "changeProfilePicture('ying')">
+            <img src="../image/avatar/zofia.png" id="zofia" alt="zofia" class="avatar_choose" onClick = "changeProfilePicture('zofia')">
+            <img src="../image/avatar/hibana.png" id="hibana" alt="hibana" class="avatar_choose" onClick = "changeProfilePicture('hibana')">
+            <br>
+            <button onClick="chooseProPicDivHide()">Cancle</button>
+            <br>
             <br>
         </div>
     </body>
---
+++ js/editprofie.js
@@ -0,0 +1,12 @@
+function chooseProPicDivShow(){
+    document.getElementById("choose_pic_div").style.visibility = 'visible';
+}
+function chooseProPicDivHide(){
+    document.getElementById("choose_pic_div").style.visibility = 'hidden';
+}
+function changeProfilePicture(id){
+    var imagePath = document.getElementById(id).src;
+    document.getElementById("avatar").src = imagePath;
+    document.getElementById("avatar1").src = imagePath;
+    chooseProPicDivHide();
+}
